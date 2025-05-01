a0a6cfd7ead9ed84ed6f3341adeaba8dd8ae0a8f
baxramovna13 <kurbanovanasiba89@gmail.com>
2025-05-01 11:53:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+// uyga vazifa
+// 1 masala
+
+// function longestPalindromicSubstring(str) {
+    
+//     let longest=""
+//     for (let i = 0; i < str.length; i++) {
+//         for (let j = i+1; j <=str.length; j++) {
+//            let part=str.slice(i,j)
+//            let reverse=part.split('').reverse().join('')
+//             if (part===reverse && part.length>=longest.length){
+//                 longest=part
+//             }
+            
+//         }
+        
+//     }
+//     return longest
+// }
+// console.log(longestPalindromicSubstring("babad"));
+
+// 2 masala
+
+// function rotateArray(arr,num) {
+//    let arr1= arr.splice(num)
+//    for (const key of arr) {
+//     arr1.push(key)
+//    }
+//     return arr1
+// }console.log(rotateArray([1,2,3,4,5],2));
+
+// 3 masala
+
+
